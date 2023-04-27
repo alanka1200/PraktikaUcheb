a056baffe6f6443a11cc0c99090b49da92f6e24e
prikol <alaninja684@gmail.com>
2023-04-27 14:26:41 +0300

    first commit

---
+++ PRATIKATEOR/Pages/CustomerPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace PRATIKATEOR.Pages
+{
+    /// <summary>
+    /// Interaction logic for CustomerPage.xaml
+    /// </summary>
+    public partial class CustomerPage : Page
+    {
+        public CustomerPage()
+        {
+            InitializeComponent();
+            List<User> users = new List<User>();
+            users.Add(new User(1, "Иванов", "Саня", 29, "Москва"));
+            users.Add(new User(2, "Ничепоренко", "Олег", 39, "Барселона"));
+            users.Add(new User(3, "Сосунков", "Глеб", 19, "Екатиренбург"));
+            UserList.ItemsSource = users;
+        }
+    }
+}
---
+++ PRATIKATEOR/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRATIKATEOR
+{
+    
+    public class User
+    {
+        public User(int Id, string fName, string Name, int age, string city)
+        {
+            this.Id = Id;
+            this.фамилия = fName;
+            this.имя = Name;
+            this.возраст = age;
+            this.город = city;
+
+        }
+
+        public int Id { get; set; }
+        public string фамилия { get; set; }
+        public string имя { get; set; }
+        public int возраст { get; set; }
+        public string город { get; set; }
+
+    }
+}
